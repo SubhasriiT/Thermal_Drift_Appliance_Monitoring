06dbdedd14cf7466deea1c4d50dcac44f7881218
Subhasri <ktsubhasri2005@gmail.com>
2026-04-16 23:16:33 +0530

    Add files via upload

---
+++ Thermal_monitor.ino
@@ -0,0 +1,24 @@
+#include "DHT.h"
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  float temp = dht.readTemperature();
+ 
+  if (isnan(temp)) {
+    Serial.println("Sensor Error");
+    delay(1000);
+    return;
+  }
+  Serial.println(temp);
+
+  delay(1000);
+}
